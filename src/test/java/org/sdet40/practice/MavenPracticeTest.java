6d0db6ec9377e17e0676751d3221d24cdfd3f785
sejal <sejalsahu9520@gmail.com>
2022-09-27 17:14:49 +0530

    commitment done

---
+++ src/test/java/org/sdet40/practice/MavenPracticeTest.java
@@ -0,0 +1,22 @@
+package org.sdet40.practice;
+
+import org.testng.annotations.Test;
+
+import com.zalando.GenericUtility.WebDriverUtility;
+
+public class MavenPracticeTest {
+	@Test
+	public void step1()
+	{
+		String browser=System.getProperty("b");
+		String url= System.getProperty("u");
+		System.out.println("browser name is-->>>" +browser);
+		System.out.println("url name is --->>>" +url);
+		System.out.println("mission passed");
+		 
+		WebDriverUtility web=new WebDriverUtility();
+		web.openBrowserwithApplication(browser, 10, url);
+		
+		
+	}
+}
